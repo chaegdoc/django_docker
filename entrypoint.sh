6d5c7842db8c978503a51da41c79a7be1503e200
Asanobm <asanobm@icloud.com>
2020-09-16 07:34:48 +0900

    update: add sleep

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+sleep 10
+
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
     while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
-      sleep 0.5
+      sleep 10
     done
 
     echo "PostgreSQL started"
